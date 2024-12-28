d0306d2b669cf7668d4ad1233b18dac30e7b76cb
rftdfedr <rftdfedr@mail.ru>
2024-12-28 10:54:41 +0300

    .

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import store from './store';
 import { Provider } from 'react-redux';
-
+import { HashRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </HashRouter>
 );
